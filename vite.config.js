624208a1d1bbeaeb463a51ae3d24e155348b18f2
zzx <452436275@qq.com>
2022-03-18 11:21:17 +0800

    hot updata

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,18 @@
 /*
  * @Author: your name
  * @Date: 2022-03-08 18:33:58
- * @LastEditTime: 2022-03-17 18:45:18
+ * @LastEditTime: 2022-03-18 11:20:58
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: /my-vue-app/vite.config.js
  */
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 
 import path from 'path'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default {
   resolve: {
     // 使用 alias 配置可以将文件打包的识别路径用别名进行替换，避免在文件引入别的组件的时候使用很长的相对路径
     alias: {
@@ -26,4 +25,4 @@ export default defineConfig({
       imports: ['vue', 'vue-router'] // 自动导入vue和vue-router相关函数
     })
   ]
-})
+}
