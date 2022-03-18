ab1b99081fb371d0c7a2b0ceeb28c7aa769c377d
zzx <452436275@qq.com>
2022-03-18 10:42:29 +0800

    ok

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2022-03-08 18:40:52
- * @LastEditTime: 2022-03-09 15:15:25
+ * @LastEditTime: 2022-03-18 10:42:24
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: /my-vue-app/.eslintrc.js
@@ -19,8 +19,52 @@ module.exports = {
     'plugin:prettier/recommended' // 合并eslint-plugin-prettier和eslint-config-prettier 配置
   ],
   globals: {
+    onActivated: 'readonly',
+    onBeforeMount: 'readonly',
+    onBeforeUnmount: 'readonly',
+    onBeforeUpdate: 'readonly',
+    onDeactivated: 'readonly',
+    onErrorCaptured: 'readonly',
+    onMounted: 'readonly',
+    onServerPrefetch: 'readonly',
+    onUnmounted: 'readonly',
+    onUpdated: 'readonly',
+    // reactivity
+    computed: 'readonly',
+    customRef: 'readonly',
+    isReadonly: 'readonly',
+    isRef: 'readonly',
+    markRaw: 'readonly',
+    reactive: 'readonly',
+    readonly: 'readonly',
+    ref: 'readonly',
+    shallowReactive: 'readonly',
+    shallowReadonly: 'readonly',
+    shallowRef: 'readonly',
+    toRaw: 'readonly',
+    toRef: 'readonly',
+    toRefs: 'readonly',
+    triggerRef: 'readonly',
+    unref: 'readonly',
+    watch: 'readonly',
+    watchEffect: 'readonly',
+    // component
+    defineComponent: 'readonly',
+    defineAsyncComponent: 'readonly',
+    getCurrentInstance: 'readonly',
+    h: 'readonly',
+    inject: 'readonly',
+    nextTick: 'readonly',
+    provide: 'readonly',
+    useCssModule: 'readonly',
+    // other
+    defineProps: 'readonly',
     defineEmits: 'readonly',
-    defineProps: 'readonly'
+    defineExpose: 'readonly',
+    withDefaults: 'readonly',
+    // vue-router
+    useRoute: 'readonly',
+    useRouter: 'readonly'
   },
   parserOptions: {
     ecmaVersion: 2021
